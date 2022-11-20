7d9e3cc1546c6754d081528f2b4a22fac63954cb
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2022-11-20 18:26:55 +0530

    Time: 3 ms (98.59%), Space: 14 MB (73.94%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -0,0 +1,70 @@
+class Solution {
+public:
+     int findpivot(vector<int>& nums)
+    {
+        int i,n=nums.size();
+
+        for(i=0;i<n-1;i++)
+        {
+            if(i+1<n)
+        {  if(nums[i]>nums[i+1])
+         return i;}
+        }
+        return -1;
+    }
+    int findtarget(int i,int j,vector<int>& nums,int target)
+    {
+        if(i>j)
+            return -1;
+        if(nums[j]==target)
+            return j;
+        if(nums[i]==target)
+            return i;
+        
+        while(i<j)
+        {
+            int mid=(i+j)/2;
+
+            if(nums[mid]==target)
+            return mid;
+            else if(nums[mid]>target)
+            j=mid;
+            else i=mid+1;
+        }
+
+       return -1;
+    }
+    bool search(vector<int>& nums, int target) {
+        
+          int n=nums.size();
+         if(n==1)
+         {
+             if(nums[0]==target)
+             return true;
+             else return false;
+         }
+         int pivot=findpivot(nums);
+         cout<<pivot<<" ";
+        if(pivot==-1)
+        {
+          int ans=findtarget(0,n-1,nums,target);
+          if(ans!=-1)
+          return true;
+         
+        }
+        else {
+             int ans1=findtarget(0,pivot,nums,target);
+            cout<<ans1<<" ";
+             if(ans1!=-1)
+        return true;
+        int ans2=findtarget(pivot+1,n-1,nums,target);
+        cout<<ans2;
+         if(ans2!=-1)
+        return true;
+      
+        }
+      
+       return false;
+        
+    }
+};
