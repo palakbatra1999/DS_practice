5e21ecdb595a74c99853b17af837e60ea5485a5c
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2025-01-19 01:52:58 +0530

    Update 0128-longest-consecutive-sequence.cpp

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,34 +1,32 @@
 class Solution {
 public:
+int search(unordered_set<int>& os, int i)
+{
+    if(os.find(i) != os.end()) 
+    {
+        return 1 + search(os,i+1);
+    } else return 0;
+
+    
+}
     int longestConsecutive(vector<int>& nums) {
-        
-         unordered_set <int> st;
-        
-        int i,n=nums.size(),m=0;
-        
-        for(i=0;i<n;i++)
-        {   
-            if (st.find(nums[i]) == st.end())
-                st.insert(nums[i]);
+
+        int m=0;
+
+        unordered_set<int> os;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            os.insert(nums[i]);
         }
-        
-         unordered_set<int> :: iterator itr;
-        
-         for (itr = st.begin(); itr != st.end(); itr++)
-         {  int c=1;
-          int inte=*itr;
-              if (st.find(inte-1) == st.end())
-              {
-                  int j=inte+1;
-                while(st.find(j) != st.end())
-                {
-                   c++; 
-                    j++;
-                }
-                  m=max(m,c);
-              }
-         }
-        
+
+        for(auto it : os)
+        {
+         if(os.find(it-1) == os.end())   
+           { m=max(m,search(os,it));}
+           
+        }
+
         return m;
         
     }
