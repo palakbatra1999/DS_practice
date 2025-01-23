b98bbbfd42869b6b47135c62ab26d0a7b6baee88
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2025-01-24 01:59:28 +0530

    Update 15-3sum.cpp

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,43 +1,44 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int n=nums.size();
-        int i,j,k;
-        vector<vector<int>> result;
-        if(n<3)
-            return result;
-        
-       
-        for(j=0;j<n;j++)
-        { 
-         i=j+1;
-         k=n-1;
-          while(i<k)
-            { int sum=nums[i]+nums[j]+nums[k];
-                if(sum==0)
-                { vector<int> res;
-                     res.push_back(nums[i]);
-                     res.push_back(nums[j]);
-                     res.push_back(nums[k]);
-                     result.push_back(res);
-                   
-                    while(i<k&&nums[i]==res[0])
-                    i++;
-                    while(i<k&&nums[k]==res[2])
-                    k--;
-                }
-                else if(sum<0)
-                    i++;
-             else k--;
-            }
-        while(j+1<n&&nums[j+1]==nums[j])
+
+    int i,j,k,l=nums.size();
+
+    vector<vector<int>> vec;
+    sort(nums.begin(),nums.end());
+
+    for(i=0;i<l;i++)
+    {
+        if(i>0 && nums[i]== nums[i-1]) continue;
+        j=i+1; k=l-1;
+
+        while(j<k)
+        {
+            int sum=nums[i]+nums[j]+nums[k];
+            if(sum == 0)
+            {
+                vector<int> temp;
+                temp.push_back(nums[i]);
+                temp.push_back(nums[j]);
+                temp.push_back(nums[k]);
+
+                vec.push_back(temp);
+
                 j++;
-         
-       
-        
+                k--;
+
+                while(j<k && nums[j]==nums[j-1]) // comparing with the last nums and moving towards k 
+                j++;
+                while(j<k && k<l && nums[k]==nums[k+1]) // comparing with the nums to  nums+ 1 and moving towards j
+                k--;
+            } else if(sum>0)   // we can't do j++ in order to make sum closer to zero, since array is sorted.. so decresing the bigger number only makes sense.
+            k--;
+            else j++; // we can't do k-- in order to make sum closer to zero, since array is sorted.. so increasing the smaller number only makes sense.
         }
+
+    }
+
+    return vec;
         
-        return result;
     }
 };
