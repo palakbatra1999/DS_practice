6c08dcb8410611743db2b99b3e8587be4b32c902
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2025-02-01 02:20:08 +0530

    Update floor-in-a-sorted-array.cpp

--- Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
+++ Floor in a Sorted Array - GFG/floor-in-a-sorted-array.cpp
@@ -1,70 +1,22 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
+ int findFloor(vector<int>& arr, int k) {
 
-
-// } Driver Code Ends
-class Solution{
-  public:
-    // Function to find floor of x
-    // n: size of vector
-    // x: element whose floor is to find
-    int findFloor(vector<long long> v, long long n, long long x){
-        
-    int left=0,right=n-1,mid;
-    
-    int c=-1;
-    if(v[n-1]<=x)
-    return n-1;
-    while(left!=right)
-    {
-        mid=(left+right)/2;
-        
-        if(v[mid]==x)
-        return mid;
-        
-        else if(v[mid]<x)
-        {
-            left=mid+1;
-            c=mid;
-        }
-        
-        else
-        {
-            right=mid;
-        }
+       int low=0,high=arr.size()-1,min=INT_MIN,ind=-1;
+       
+       while(low<=high)
+       {
+           int mid=(low+high)/2;
+           
+           if(arr[mid]==k)
+           return mid;
+           else if(arr[mid]<k)
+           {
+              if(min<arr[mid])
+              {min=arr[mid];
+                  ind=mid;
+              }
+               low=mid+1;
+           }else high=mid-1;
+       }
+       
+       return ind;
     }
-    return c;    
-        
-    }
-};
-
-
-//{ Driver Code Starts.
-
-int main() {
-	
-	long long t;
-	cin >> t;
-	
-	while(t--){
-	    long long n;
-	    cin >> n;
-	    long long x;
-	    cin >> x;
-	    
-	    vector<long long> v;
-	    
-	    for(long long i = 0;i<n;i++){
-	        long long temp;
-	        cin >> temp;
-	        v.push_back(temp);
-	    }
-	    Solution obj;
-	    cout << obj.findFloor(v, n, x) << endl;
-	   
-	}
-	
-	return 0;
-}
-// } Driver Code Ends
