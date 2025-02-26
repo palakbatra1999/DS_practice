71eaf565419d997b64b46eecfa6aabc8df9bcffb
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2025-02-26 21:39:04 +0530

    Update 560-subarray-sum-equals-k.cpp

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        map<int, int> pSum;  // Map to store prefix sum frequencies
+        pSum[0] = 1;  // Initial condition (helps when sum itself is k)
 
-        map<int,int> pSum;
+        int sum = 0, count = 0;
 
-        pSum[0]=1;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];  // Update the prefix sum
 
-        int sum=0, count=0;
-
-        for(int i=0;i<nums.size();i++)
-        {
-            sum+=nums[i];
-
-            if(pSum.find(sum-k)!=pSum.end())
-            {
-               count+=pSum[sum-k];
+            if (pSum.find(sum - k) != pSum.end()) {  
+                count += pSum[sum - k];  // Add frequency of (sum - k) found earlier
             }
 
-            pSum[sum]++;
+            pSum[sum]++;  // Store the prefix sum in the map
         }
 
         return count;
-        
     }
 };
