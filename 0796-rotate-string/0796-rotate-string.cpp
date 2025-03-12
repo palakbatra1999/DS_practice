1426c77b038afaf4c1d62bc54308ea1e04634312
palakbatra1999 <32217364+palakbatra1999@users.noreply.github.com>
2025-03-12 23:35:31 +0530

    Update 0796-rotate-string.cpp

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,33 +1,13 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        int i,l=s.length(),n=goal.length(),index=0;
-        if(l!=n)
+        // Length check is mandatory
+        if (s.length() != goal.length()) {
             return false;
-        
-        vector<int> v;
-        char c=goal[0];
-        
-        for(i=0;i<n;i++)
-        {
-            if(s[i]==c)
-              { 
-                v.push_back(i);
-              //  cout<<i<<" ";
-               //  break;
-              }
         }
-      
-        for(i=0;i<v.size();i++)
-      { 
-            int index=v[i];
-           
-            if(s.substr(index,index+l)+s.substr(0,index)==goal)
-           return true;
-       // cout<<p;
-      }
-       
-       // else
-            return false;
+
+        // Concatenation trick — contains all possible rotations
+        string combined = s + s;
+        return combined.find(goal) != string::npos;
     }
 };
